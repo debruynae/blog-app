e9329aca9703d265a5db9b0b84b9835fb15859c3
Rhushabh <luffsnape@gmail.com>
2022-04-04 10:22:49 +0530

    Heroku config files updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-c&l(*bw(pocbn&q#t+k^jh#$%^2gpzhdn$$$!m3ysv@t2(nbfl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
